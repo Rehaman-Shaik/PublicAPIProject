715da333e101daf64e92a4189f115b54404b8b02
unknown <shaik.rehamanrp@gmail.com>
2024-05-15 21:45:41 +0530

    added weather api route

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.get('/jokes_api', async (req, res) => {
     if (response.data.type == "twopart") {
         let setup = response.data.setup
         res.render("api.ejs", {
+            type: "Jokes",
             setup: setup,
             delivery: response.data.delivery
         })
@@ -28,7 +29,24 @@ app.get('/jokes_api', async (req, res) => {
     if (response.data.type === "single") {
         let singlepart = response.data.joke
         res.render("api.ejs", {
+            type: "Jokes",
             singlepart: singlepart
         })
     }
+})
+
+
+app.get("/weather_api", async(req,res)=>{
+    let response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${12.994147}&lon=${77.699434}&appid=${"541c962a9d121f175c3013075c3c54e4"}`)
+    let temp = response.data.main.temp - 273
+    let data = {
+        city : response.data.name,
+        wind : response.data.wind,
+        main :response.data.main,
+        temp : Math.floor(temp)
+    }
+    res.render("api.ejs",{
+        type: "Weather",
+        data:data
+    })
 })
