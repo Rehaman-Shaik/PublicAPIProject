89e991ad63794e499a66ca513564dfecb953d147
unknown <shaik.rehamanrp@gmail.com>
2024-05-15 18:40:59 +0530

    compelted the joke api somewhat

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ import express from "express";
 import axios from "axios";
 
 
-const app  = express();
+const app = express();
 let port = 3000;
 app.use(express.static("public"))
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server started successfully on port ${port}`)
 })
 
 app.get('/', (req, res) => {
     res.render("index.ejs")
-  })
+})
 
-app.get('/jokes_api', async (req,res)=>{
+app.get('/jokes_api', async (req, res) => {
     let response = await axios.get("https://v2.jokeapi.dev/joke/Any")
-    response = JSON.stringify(response.data)
-    res.render("api.ejs", {
-        content : response
-    })
+    console.log(response.data)
+    if (response.data.type == "twopart") {
+        let setup = response.data.setup
+        res.render("api.ejs", {
+            setup: setup,
+            delivery: response.data.delivery
+        })
+    }
+    if (response.data.type === "single") {
+        let singlepart = response.data.joke
+        res.render("api.ejs", {
+            singlepart: singlepart
+        })
+    }
 })
